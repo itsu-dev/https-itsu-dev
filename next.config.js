7d3c5b81254941a8619efc21e4d7877831b3b658
itsu-dev <itsu020402@gmail.com>
2023-11-23 19:35:11 +0900

    fix: bugs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withLinaria = require('next-with-linaria');
-const nextConfig = {};
+const nextConfig = {
+  output: 'export'
+};
 module.exports = withLinaria(nextConfig);
